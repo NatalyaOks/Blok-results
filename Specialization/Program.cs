667e9cb50ff4b4d66f12f8d5d65acf869c82fb78
Natalya Oksina <natevg@yandex.ru>
2022-09-05 12:53:43 +0300

    Добавлена рабочая программа

--- Specialization/Program.cs
+++ Specialization/Program.cs
@@ -1,9 +1,26 @@
-﻿string [] array = {"1234","qwert","285","7a","-2"};
+﻿string[] array = {"!", "1234", "qwert", "285", "7a", "-2","итоги"};
+int lenArray = 0;
 
 for (int i = 0; i < array.Length; i++)
 {
     if (array[i].Length <= 3)
     {
-        Console.Write (array[i] + "\t");
+        lenArray++;
     }
+}
+
+string[] array1 = new string[lenArray];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        array1[j] = array[i];
+        j++;
+    }
+}
+
+for (int i = 0; i < array1.Length; i++)
+{
+    Console.Write(array1[i] + " ");
 }

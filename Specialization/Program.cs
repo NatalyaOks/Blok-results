7475c9ab693e46fed76e16788298d4089c2846e7
Natalya Oksina <natevg@yandex.ru>
2022-09-05 12:06:24 +0300

    В файл добавлен вывод элементов массива

--- Specialization/Program.cs
+++ Specialization/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] array = {"1234","qwert","285","7a","-2"};
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.Write (array[i] + "\t");
+    }
+}
